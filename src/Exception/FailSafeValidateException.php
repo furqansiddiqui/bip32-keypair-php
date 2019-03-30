aeeb98c89f7fe8cbc27a31e3ee46b015332e4d22
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-03-30 18:51:41 +0500

    Fail-safe exception

---
+++ src/Exception/FailSafeValidateException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is a part of "furqansiddiqui/bip32-keypair-php" package.
+ * https://github.com/"furqansiddiqui/bip32/bip32-keypair-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/"furqansiddiqui/bip32/bip32-keypair-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\BIP32\Exception;
+
+/**
+ * Class FailSafeValidateException
+ * @package FurqanSiddiqui\BIP32\Exception
+ */
+class FailSafeValidateException extends PublicKeyException
+{
+}
