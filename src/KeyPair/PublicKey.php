377b1cf8f0729176f86578fc59c1d43dffeeb0fb
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-03-30 18:46:56 +0500

    Public keys

---
+++ src/KeyPair/PublicKey.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * This file is a part of "furqansiddiqui/bip32-keypair-php" package.
+ * https://github.com/"furqansiddiqui/bip32/bip32-keypair-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/"furqansiddiqui/bip32/bip32-keypair-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\BIP32\KeyPair;
+
+use FurqanSiddiqui\BcMath\BcBaseConvert;
+use FurqanSiddiqui\BIP32\ECDSA\Curves;
+use FurqanSiddiqui\BIP32\ECDSA\Vectors;
+use FurqanSiddiqui\BIP32\Exception\PublicKeyException;
+use FurqanSiddiqui\DataTypes\Base16;
+use FurqanSiddiqui\DataTypes\Binary;
+use FurqanSiddiqui\ECDSA\Vector;
+
+/**
+ * Class PublicKey
+ * @package FurqanSiddiqui\BIP32\KeyPair
+ */
+class PublicKey
+{
+    /** @var PrivateKey */
+    private $privateKey;
+    /** @var int */
+    private $curve;
+    /** @var \FurqanSiddiqui\ECDSA\Vector */
+    private $vector;
+    /** @var Binary */
+    private $publicKey;
+    /** @var Binary */
+    private $compressedPublicKey;
+
+    /**
+     * PublicKey constructor.
+     * @param PrivateKey $keyPair
+     * @throws PublicKeyException
+     * @throws \FurqanSiddiqui\ECDSA\Exception\GenerateVectorException
+     * @throws \FurqanSiddiqui\ECDSA\Exception\MathException
+     */
+    public function __construct(PrivateKey $keyPair)
+    {
+        $this->privateKey = $keyPair;
+        $this->curve = $this->privateKey->getEllipticCurve();
+        if (!$this->curve) {
+            throw new PublicKeyException('Cannot generate public key; ECDSA curve is not defined');
+        }
+
+        $this->vector = Vectors::Curve($this->curve, $this->privateKey->raw());
+        switch ($this->curve) {
+            case Curves::SECP256K1:
+            case Curves::SECP256K1_OPENSSL:
+                $coords = $this->vector->coords();
+                if (!$coords->x()) {
+                    throw new PublicKeyException('Secp256k1 curve missing "x" point');
+                } elseif (!$coords->y()) {
+                    throw new PublicKeyException('Secp256k1 curve missing "y" point');
+                }
+
+                $base16x = $coords->x()->encode(false);
+                $base16y = $coords->y()->encode(false);
+                $bitwise = BcBaseConvert::BaseConvert($base16y, 12, 2);
+                $sign = substr($bitwise, -1) === "0" ? "02" : "03";
+                $this->publicKey = (new Base16($base16x . $base16y))->readOnly(true);
+                $this->compressedPublicKey = (new Base16($sign . $base16x))->readOnly(true);
+                break;
+            default:
+                throw new PublicKeyException(
+                    sprintf('Not sure how to convert "%s" vector into public key', Curves::INDEX[$this->curve])
+                );
+
+        }
+    }
+
+    /**
+     * @return Binary
+     */
+    public function raw(): Binary
+    {
+        return $this->publicKey;
+    }
+
+    /**
+     * @return Binary
+     * @throws PublicKeyException
+     */
+    public function compressed(): Binary
+    {
+        if (!$this->compressedPublicKey) {
+            throw new PublicKeyException(
+                sprintf('Could not generate a compressed public key using "%s" curve', Curves::INDEX[$this->curve])
+            );
+        }
+
+        return $this->compressedPublicKey;
+    }
+
+    /**
+     * @param int $prefix
+     * @return P2PKH_Address
+     * @throws AddressGenerateException
+
+    public function p2pkh(int $prefix): P2PKH_Address
+    {
+        return new P2PKH_Address($this, $prefix);
+    }*/
+
+    /**
+     * @param int $p2pkhPrefix
+     * @param int $p2shPrefix
+     * @return P2SH_Address
+     * @throws AddressGenerateException
+
+    public function p2sh(int $p2pkhPrefix, int $p2shPrefix): P2SH_Address
+    {
+        return new P2SH_Address($this->p2pkh($p2pkhPrefix), $p2shPrefix);
+    }*/
+
+    /**
+     * @return FailSafeCurveValidate
+
+    public function failSafeCurveValidate(): FailSafeCurveValidate
+    {
+        return new FailSafeCurveValidate($this);
+    }*/
+
+    /**
+     * @return int
+     */
+    public function curve(): int
+    {
+        return $this->curve;
+    }
+
+    /**
+     * @return Vector
+     */
+    public function vector(): Vector
+    {
+        return $this->vector;
+    }
+
+    /**
+     * @return PrivateKey
+     */
+    public function privateKey(): PrivateKey
+    {
+        return $this->privateKey;
+    }
+}
